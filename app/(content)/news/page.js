0af703a9229c1917b212c46ab7a00c237ae11751
dmr-911 <dewan.mizanur911@gmail.com>
2024-05-30 23:52:33 +0600

    feat: removed external backend, used direct database from RSC

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,17 +1,18 @@
 import React from "react";
 import NewsList from "@/components/news/news-list";
+import { getAllNews } from "@/lib/news";
 
 const News = async () => {
-  const response = await fetch("http://localhost:8080/news");
-  if (!response.ok) {
-    throw new Error("Failed to fetch news.");
-  }
+  // const response = await fetch("http://localhost:8080/news");
+  // if (!response.ok) {
+  //   throw new Error("Failed to fetch news.");
+  // }
 
-  const news = await response.json();
+  // const news = await response.json();
   return (
     <div>
       <h1>News Page</h1>
-      <NewsList news={news} />
+      <NewsList news={getAllNews()} />
     </div>
   );
 };
